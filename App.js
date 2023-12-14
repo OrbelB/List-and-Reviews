3fbfbc30708ea4468b7e5c1576a45d281766cdf0
OrbelB <o.baghdasian@gmail.com>
2023-12-13 21:00:47 -0800

    Updated App.js & HomeScreen -- commented in the features that will be included in the app
    -- Could not get wep app to function, will focus on mobile development first

--- App.js
+++ App.js
@@ -1,24 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View, StyleSheet, Text, SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { Provider } from 'react-redux'
+// import { Provider } from 'react-redux';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import {store} from './store'
+// import { store } from './store';
 import HomeScreen from './Screens/HomeScreen';
-
 const Stack = createNativeStackNavigator();
+
+const linking = {
+  prefixes: ['localhost'],
+  config: {
+    screens: {
+      Home: '',
+      Profile: ':id/profile',
+      Settings: ':id/blog',
+    }
+  },
+};
+
+// Provider
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Provider store={store}>
+      <NavigationContainer linking={linking}>
         <Stack.Navigator>
-          <Stack.Screen name = "Home" component={HomeScreen}/>
+          <Stack.Screen name="List and Reviews" component={HomeScreen} />
+          {/* Add more screens as needed */}
+          {/* Profile */}
+            {/* user picture */}
+            {/* user name */}
+            {/* number of lists */}
+            {/* number of reviews */}
+          {/* Lists - hub for all of the list the user has*/}
+            {/* List - A specific list, scroll through the list and see the rankings */}
+            {/* List settigs - edit the ranking system */}
+              {/* Reviews - a specific review within a list */}
+          {/* share a List */}
+            {/* Share to user */}
+            {/* Share theough text */}
+            {/* other shharing options? */}
+          {/* Search for a review */}
+            {/* Search using title */}
+          {/* Settings */}
+            {/* can edit username, email, change password, delete account */}
+          {/* Login Page */}
+            {/* log in through google, or email / password */}
         </Stack.Navigator>
-      </Provider>
-    </NavigationContainer>
+      </NavigationContainer>
   );
 }
-
--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -5,7 +5,13 @@ const HomeScreen = () => {
   console.log("HELLO world");
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text>View Profile</Text>
+      <Text>Create a New List</Text>
+      <Text>View Lists</Text>
+      <Text>Search for Review</Text>
+      <Text>Share List</Text>
+      
+
     </View>
   );
 };
